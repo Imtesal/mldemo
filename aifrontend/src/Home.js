74c2a56d743dff510de8146148f742c63748e4fa
Amit-sood <amit.sood@globallogic.com>
2022-12-06 14:51:37 +0530

    Update Home.js

--- aifrontend/src/Home.js
+++ aifrontend/src/Home.js
@@ -54,7 +54,7 @@ function Home() {
         onDemoClick={() => demoHandler("auto")}
         link="/duplicate"
         title="Matching & Pinning"
-        description="Solve duplicate records problem (address, name etc.) using machine learning in Matching & Pinning Process"
+        description="Solve duplicate records problem using machine learning in Matching & Pinning Process"
       />
       <Card
         icon={faChartGantt}
